d5823697d795048630d2b8cfd908542354e0b241
Marc026 <mlapira@horizon.csueastbay.edu>
2023-04-19 18:55:41 -0700

    Started working on E.C.

--- FamilySet.cpp
+++ FamilySet.cpp
@@ -20,3 +20,51 @@ vector<string> FamilySet::getWords() {
     return *wordlist;
 }
 
+FamilySet::FamilySet(string dictFile, int len) {
+    ifstream file(dictFile);
+
+    // Allocate memory for the vector
+    wordlist = new vector<string>;
+
+    // Read words from the file and add them to the vector if they have the required length
+    string word;
+    while (file >> word) {
+        if (word.length() == len) {
+            wordlist->push_back(word);
+        }
+    }
+
+    // Close the file
+    file.close();
+}
+
+FamilySet::~FamilySet() {
+
+}
+
+void FamilySet::filterFamilies(string letter, string guessPattern) {
+
+}
+
+void FamilySet::setFamily(string family) {
+    if (dictionaries.count(family) > 0) {
+        wordlist = dictionaries[family];
+        dictionaries.clear();
+    }
+    else{
+        return;
+    }
+}
+
+int FamilySet::familySize(string family) {
+    return 0;
+}
+
+void FamilySet::resetFamilyIter() {
+
+}
+
+string FamilySet::getNextFamily() {
+    return std::string();
+}
+
